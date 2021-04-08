64f756201f8c605bdba6e738ea64deee0badec8e
James DePaola <james.depaola.268@my.csun.edu>
2021-04-07 20:09:15 -0700

    more stuff

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, James!!! GOOD MORNING!!");
-=======
-
-<<<<<<< HEAD
+        System.out.println("Hello, James!!! WTF?!!");
         System.out.println("Hello, World! from Ian");
-=======
         System.out.println("Hello, World! from Varoujan");
-
         System.out.println("Hello, World!");
         System.out.println("My name is Colin");
->>>>>>> ed26890b7ec8691fb32faac83a38e53e6610e86b
->>>>>>> 0435c163f4fde85920326c91e3d952b076074300
     }
 }
