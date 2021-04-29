72cf34506ae370a27f7a9b7528dba62f7a5cbb55
Ian Muldoon <ian.muldoon22@gmail.com>
2021-04-29 13:29:40 -0700

    Ian - added Kings and King movement

--- src/Piece.java
+++ src/Piece.java
@@ -4,6 +4,22 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.*;
 import javafx.scene.shape.Ellipse;
 
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javafx.scene.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+
 public class Piece extends StackPane {
     public int _playerID;
     public int _row;
@@ -26,10 +42,13 @@ public class Piece extends StackPane {
         this._isKing = false;
     }
 
-    public void CreateCircle()
-    {
+    public void CreateCircle() {
+        Text t = new Text();
+        t.setText("K");
+        t.setFont(Font.font("Verdana", 20));
+
         Position position = Position.getPieceXY(this);
-        move(position._x , position._y );
+        move(position._x, position._y);
 
         Ellipse circle = new Ellipse(Main.TILE_SIZE * 0.3125, Main.TILE_SIZE * 0.26);
         if (type == PieceType.RED)   //Depending on type, fill circle red or white
@@ -40,9 +59,16 @@ public class Piece extends StackPane {
         circle.setTranslateX((Main.TILE_SIZE - Main.TILE_SIZE * 0.3125 * 2) / 2);
         circle.setTranslateY((Main.TILE_SIZE - Main.TILE_SIZE * 0.26 * 2) / 2);
 
-        this.getChildren().addAll(circle);
-    }
 
+        if (_isKing) {
+            t.setTranslateX((Main.TILE_SIZE - Main.TILE_SIZE * 0.3125 * 2) / 2);
+            t.setTranslateY((Main.TILE_SIZE - Main.TILE_SIZE * 0.26 * 2) / 2);
+            this.getChildren().addAll(circle, t);
+        }else{
+
+            this.getChildren().addAll(circle);
+        }
+    }
     public void move(int x, int y)
     {
         oldX = x * Main.TILE_SIZE;
