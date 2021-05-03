d06e78f00c4a556d6b6612b057c3e2acbeebcd92
James DePaola <james.depaola.268@my.csun.edu>
2021-05-03 11:23:41 -0700

    Created classes to interact with history file.

---
+++ src/HistoryRecord.java
@@ -0,0 +1,23 @@
+public class HistoryRecord
+{
+    public int ID;
+    public String Name;
+    public int Wins;
+    public int Losses;
+
+    public HistoryRecord(int ID, String name, int wins, int losses)
+    {
+        this.ID = ID;
+        this.Name = name;
+        this.Wins = wins;
+        this.Losses = losses;
+    }
+
+    public HistoryRecord(String[] values)
+    {
+        this.ID = Integer.parseInt(values[0]);
+        this.Name = values[1];
+        this.Wins = Integer.parseInt(values[2]);
+        this.Losses = Integer.parseInt(values[3]);
+    }
+}
