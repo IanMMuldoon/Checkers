35cf9c3fdcf96903bd98ccaec6298f7bd83ce784
ColinDShepard <ColinDShepard@gmail.com>
2021-05-05 20:57:57 -0700

    Added FXML

---
+++ src/MainMenuController.java
@@ -0,0 +1,14 @@
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+    public Button playButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
